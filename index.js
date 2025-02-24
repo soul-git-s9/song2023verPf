d16b929fa1fa57dcaa2f0c823ddcf8618203b75a
May <expert_ss@naver.com>
2025-02-24 21:13:21 +0900

    인덱스 js 수정

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-$(function(){
+$(function() {
+    var images = [
+        { normal: 'img/about.png', hover: 'img/bg_about.png' },
+        { normal: 'img/portfolio.png', hover: 'img/bg_portfolio.png' },
+        { normal: 'img/contact.png', hover: 'img/bg_contact.png' }
+    ];
+
     $('.con li').each(function(index) {
-        var images = [
-            { normal: 'img/about.png', hover: 'img/bg_about.png' },
-            { normal: 'img/portfolio.png', hover: 'img/bg_portfolio.png' },
-            { normal: 'img/contact.png', hover: 'img/bg_contact.png' }
-        ];
+        $(this).on('click', function() {
+            // 모든 li에서 active 제거 후 현재 li에만 추가
+            $('.con li').removeClass('active');
+            $(this).addClass('active');
 
-        $(this).mouseenter(function(){
+            // 이미지 변경
+            $('.con li img').each(function(i) {
+                $(this).attr('src', images[i].normal);
+            });
             $(this).find('img').attr('src', images[index].hover);
-        });
-
-        $(this).mouseleave(function(){
-            $(this).find('img').attr('src', images[index].normal);
         });
     });
 });
