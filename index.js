7693cb30ee2d4c01b7057f5f99e734b75a96c718
May <expert_ss@naver.com>
2025-02-24 20:43:08 +0900

    인덱스 hover 수정

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 $(function(){
-	$('.con li:nth-of-type(1)').mouseenter(function(){
-		 $('.con li:nth-of-type(1) img').attr('src','img/bg_about.png');
-	});
+    $('.con li').each(function(index) {
+        var images = [
+            { normal: 'img/about.png', hover: 'img/bg_about.png' },
+            { normal: 'img/portfolio.png', hover: 'img/bg_portfolio.png' },
+            { normal: 'img/contact.png', hover: 'img/bg_contact.png' }
+        ];
 
-	$('.con li:nth-of-type(1)').mouseleave(function(){
-		 $('.con li:nth-of-type(1) img').attr('src','img/about.png');
-	});
+        $(this).mouseenter(function(){
+            $(this).find('img').attr('src', images[index].hover);
+        });
 
-
-		$('.con li:nth-of-type(2)').mouseenter(function(){
-		 $('.con li:nth-of-type(2) img').attr('src','img/bg_portfolio.png');
-	});
-
-	$('.con li:nth-of-type(2)').mouseleave(function(){
-		 $('.con li:nth-of-type(2) img').attr('src','img/portfolio.png');
-	});
-
-
-		$('.con li:nth-of-type(3)').mouseenter(function(){
-		 $('.con li:nth-of-type(3) img').attr('src','img/bg_contact.png');
-	});
-
-	$('.con li:nth-of-type(3)').mouseleave(function(){
-		 $('.con li:nth-of-type(3) img').attr('src','img/contact.png');
-	});
+        $(this).mouseleave(function(){
+            $(this).find('img').attr('src', images[index].normal);
+        });
+    });
 });
